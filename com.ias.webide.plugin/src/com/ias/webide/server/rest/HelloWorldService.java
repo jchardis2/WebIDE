18158286e34e51601245bc9cc04b547f29404367
Jimmy Hardison <jchardis2@gmail.com>
2014-09-08 01:20:07 -0400

    jersey working

--- com.ias.webide.plugin/src/com/ias/webide/server/rest/HelloWorldService.java
+++ com.ias.webide.plugin/src/com/ias/webide/server/rest/HelloWorldService.java
@@ -1,15 +1,20 @@
 package com.ias.webide.server.rest;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/hello")
+@Path("/db")
 public class HelloWorldService {
 
 	@GET
 	@Path("/{param}")
+	@Produces(MediaType.TEXT_PLAIN)
 	public Response getMsg(@PathParam("param") String msg) {
 
 		String output = "Jersey say : " + msg;
@@ -18,4 +23,10 @@ public class HelloWorldService {
 
 	}
 
+	@Path("smooth")
+	@GET
+	public Response smooth(@DefaultValue("2") @QueryParam("step") int step, @DefaultValue("true") @QueryParam("min-m") boolean hasMin,
+			@DefaultValue("true") @QueryParam("max-m") boolean hasMax, @DefaultValue("true") @QueryParam("last-m") boolean hasLast) {
+		return null;
+	}
 }
